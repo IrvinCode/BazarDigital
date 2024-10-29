667fa513669e47f1a439a34bffa38ebf2479cc40
Irvin Garcia <irvingarcia@Irvins-MacBook-Pro.local>
2024-10-28 19:16:44 -0700

    New products can now be added

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/products', to: 'products#index'
-  get '/products/:id', to: 'products#show', as: :product
+  post "products", to: "products#create"
+  get "products/new", to: "products#new", as: :new_product
+  get "products", to: "products#index"
+  get "products/:id", to: "products#show", as: :product
 end
